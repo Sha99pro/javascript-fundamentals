b9edfdeea8096c255c0a5ff2c08c56a567a04451
meghcodes <marellameghana99@gmail.com>
2026-05-29 11:29:07 -0500

    add code lines to arrow functions

--- tutorials/09-more-functions/arrow-syntax/exercises/demo.js
+++ tutorials/09-more-functions/arrow-syntax/exercises/demo.js
@@ -6,16 +6,25 @@
 function traditionalCalculateFuel(mass) {
 	return mass * 11.2;
 }
+    
 
 // STAGE 2: Block-Body Arrow Function (Explicit Return)
 
 // DEMO: Rewrite the traditional function with transitional arrow syntax;
 
+const blockBodyCalculateFuel = mass => {
+    return mass * 11.2
+}
+console.log(`Explicit return: ${blockBodyCalculateFuel(450)}`);
+
 // STAGE 3: Concise-Body Arrow Function (Implicit Return)
 
 // DEMO: Rewrite the function again with concise array syntax
-
+const conciseBodyCalculateFuel = mass => mass * 11.2
+console.log(`Implicit return: ${conciseBodyCalculateFuel(45)}`);
 // DEMO: Test the concise arrow function
+
+
 
 /** EXAMPLE 2 - THE HOISTING PARADOX **/
 
@@ -25,12 +34,14 @@ function readTraditionalSensor() {
 	return "Telemetry Active";
 }
 
+console.log(`Traditional answer: ${readTraditionalSensor()}`);
 // 2. DEMO: On the next line, call the arrow function defined below
 
 const readArrowSensor = () => "Telemetry Active";
 
+console.log(`Arrow answer: ${readArrowSensor()}`);
 // DEMO: Call the arrow function again on the next line.
-
+console.log(`Arrow answer: ${readArrowSensor()}`);
 // DEMO: Make a git commit!
 
 /* 

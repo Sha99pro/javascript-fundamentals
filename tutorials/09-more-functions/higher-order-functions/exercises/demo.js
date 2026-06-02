3d0ce1179d7a783d36b1b34f6761340862c5df71
meghcodes <marellameghana99@gmail.com>
2026-06-01 23:55:33 -0500

    Add higher-order function examples

--- tutorials/09-more-functions/higher-order-functions/exercises/demo.js
+++ tutorials/09-more-functions/higher-order-functions/exercises/demo.js
@@ -5,15 +5,33 @@
 // DEMO: Define a HOF that accepts a target name and a function that can describe
 // specifically how that target is engaged.
 
+function engageTacticalComputer(targetName, engageTarget) {
+  console.log(engageTarget(targetName));
+}
+
+
 // DEMO: Pass an in-place contractor to SCAN the target
 
+engageTacticalComputer("Unidentified Frigate", (name) => "Scanning " + name);
+
 // DEMO: Pass a completely different contractor to WEAPON LOCK the target
+
+engageTacticalComputer("Unknown target", (name) => "Locking" + name);
+
 
 /** EXAMPLE 2 - RETURNING A FUNCTION FROM A HOF **/
 
 // DEMO: Define an HOF that can manufacture and return brand-new specialized functions
+function manufacturer(operation){
+    return (a , b) =>{
+            return operation(a , b);
+    }
+}
 
 // DEMO: Use the HOF to manufacture two distinct calculation engines
+
+const addEngine = manufacturer((a , b) => a + b);
+const multiplyEngine = manufacturer((a, b) => a * b);
 
 // DEMO: Call each manufactored function and log a report:
 

04c37653bf1bac89e383bf0b0fa47092a77dead3
meghcodes <marellameghana99@gmail.com>
2026-06-02 10:18:22 -0500

    Add code lines to REDUCING: PRACTICE

--- tutorials/09-more-functions/reducing/exercises/practice.js
+++ tutorials/09-more-functions/reducing/exercises/practice.js
@@ -15,6 +15,12 @@ const torpedoCrateWeightsKg = [120, 150, 95, 110];
     Capture the final calculation in a variable named 'totalWeaponsMass' and log it.
 */
 
+const totalWeaponsMass = torpedoCrateWeightsKg.reduce((acc , current) => {
+    return acc + current;
+}, 0);
+console.log(totalWeaponsMass);
+
+
 /** EXERCISE 2: SECTOR SURVEY POPULATION SUMMARY **/
 
 const sectorHabitationHubs = [
@@ -29,3 +35,7 @@ const sectorHabitationHubs = [
     population across the entire sector.
     Store the final number in a constant named 'totalSectorPopulation' and log it.
 */
+const totalSectorPopulation = sectorHabitationHubs.reduce((acc , hub) =>{
+    return acc + hub.populationCount;
+}, 0);
+console.log(totalSectorPopulation);

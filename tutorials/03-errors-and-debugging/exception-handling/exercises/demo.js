994b62cb0cd46bce9df2281838bff06a06121d06
meghcodes <marellameghana99@gmail.com>
2026-06-10 19:12:05 -0500

    Add ttry/catch/finally statements to the existing code.

--- tutorials/03-errors-and-debugging/exception-handling/exercises/demo.js
+++ tutorials/03-errors-and-debugging/exception-handling/exercises/demo.js
@@ -3,24 +3,22 @@
 /** EXAMPLE 1 - DEFENDING AGAINST A FATAL ACCIDENT **/
 
 // Let's create a simulated environment switch
+
 let isBackupGeneratorOnline = false;
+try{
+    console.log("Entering System Check...");
 
-console.log("Entering System Check...");
-
-// DEMO: Run the file to see what happens when we try to print an undefined variable.
-console.log(isCargoBayPressurized);
-
-console.log("This line will never run because the line above broke!");
-
+    // DEMO: Run the file to see what happens when we try to print an undefined variable.
+    console.log(isCargoBayPressurized);
 // DEMO: Create a try block and move the three console.log statements inside it.
 // Run the file again. What happens now?
-
-// DEMO: Create a catch block to capture the error object and print a warning
-// with the message from the error object.
-
-// DEMO: Create a finally block to ensure certain code is run no matter what.
-// Print a closing remark.
-
+    console.log("This line will never run because the line above broke!");
+ } catch(errObj){
+   console.log("Warning!, critical problem detected."); 
+   console.log(`Error : ${errObj.message}`);  
+} finally{
+    console.log("Closing remark");
+}
 console.log("\n🚀 Success! The script did not crash and is still running.");
 
 // DEMO: Make a git commit!
@@ -29,3 +27,7 @@ console.log("\n🚀 Success! The script did not crash and is still running.");
     Follow up with additional exercises after demo for hands-on practice 
     with problem-solving and coding!
 */
+// DEMO: Create a catch block to capture the error object and print a warning
+// with the message from the error object.
+// // DEMO: Create a finally block to ensure certain code is run no matter what.
+// Print a closing remark.
